2c65cca3583db9e7308f0e37858414dc543fcb39
Cooper Sumrall <serunde@yahoo.com>
2018-08-27 14:49:46 -0700

    initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var y = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
+
+        transform.Translate(y, 0, 0);
+        transform.Translate(0, 0, z);
+    }
+}
